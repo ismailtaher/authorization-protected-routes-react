4519aebcbb0672bc90374f1cbf274254cd2be2d0
ismailtaher <ismail.taher@ymail.com>
2025-01-14 18:41:29 +0500

    Added all the components & their routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import Register from "./Register";
-import Login from "./Login";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Admin from "./components/Admin";
+import Editor from "./components/Editor";
+import Home from "./components/Home";
+import LinkPage from "./components/LinkPage";
+import Lounge from "./components/Lounge";
+import Missing from "./components/Missing";
+import Unauthorized from "./components/Unauthorized";
+import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <main className="App">
-      <Login />
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* public routes */}
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="linkpage" element={<LinkPage />} />
+        <Route path="unauthorized" element={<Unauthorized />} />
+
+        {/* Routes we want to protect */}
+        <Route path="/" element={<Home />} />
+        <Route path="editor" element={<Editor />} />
+        <Route path="admin" element={<Admin />} />
+        <Route path="lounge" element={<Lounge />} />
+
+        {/* catch all */}
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
   );
 }
 
